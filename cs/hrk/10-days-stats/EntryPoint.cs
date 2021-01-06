d25d22d334ff4ada7030e175540721631f959dc8
russki007 <dmitrey.makarov@gmail.com>
2021-01-07 09:57:11 +1300

    HRK: 10 days of stats, 1

---
+++ cs/hrk/10-days-stats/EntryPoint.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace hrk {
+    class EntryPoint {
+        private static readonly Assembly asm = Assembly.GetAssembly(typeof(EntryPoint));
+        private static readonly string mainTest = "MainTest";
+        private static readonly string nameSpace = typeof(EntryPoint).Namespace;
+git
+
+            return allNames;
+        }
+
+        private static MethodInfo GetMainTest(string className) {
+            string fullName;
+            if (!className.Contains(nameSpace))
+                fullName = nameSpace + "." + className;
+            else
+                fullName = className;
+
+            Type type = asm.GetType(fullName);
+            MethodInfo testMethod = type.GetMethod(mainTest);
+            return testMethod;
+        }
+
+
+        private static void ListTestCases() {
+            try {
+                SortedSet<string> classNames = GetAllTestClasses();
+
+                Console.Write(@"
+==========================================
+TEST CASES
+==========================================
+");
+                int i = 1;
+                foreach (var name in classNames) {
+                    Console.WriteLine($" {i++}. {name}");
+                }
+
+                Console.WriteLine();
+            }
+            catch (Exception ex) {
+                string inner = ex.Message;
+                Console.WriteLine("Error: {0}", inner);
+            }
+        }
+
+        [STAThread]
+        static void Main(string[] args) {
+            CommandLineApplication app = new CommandLineApplication(throwOnUnexpectedArg: true);
+            var test = app.Option("-t | --test <class>", "Test Class", CommandOptionType.SingleValue);
+            var list = app.Option("-l | --list", "List Test Class", CommandOptionType.NoValue);
+            app.HelpOption("-? | -h | --help");
+            app.OnExecute(() => {
+                if (!list.HasValue() && !test.HasValue()) {
+                    app.ShowHelp();
+                }
+                else if (list.HasValue()) {
+                    ListTestCases();
+                }
+                else {
+                    try {
+                        // test method invocation
+                        string className = test.Value();
+                        MethodInfo testMethod = GetMainTest(className);
+                        if (testMethod != null) {
+                            string[] testArgs = new string[args.Length];
+                            for (int i = 0; i < args.Length; i++) {
+                                testArgs[i] = args[i];
+                            }
+
+
+                            testMethod.Invoke(null, new object[] { testArgs });
+                        }
+                        else
+                            Console.WriteLine("No demo test for: {0}", className);
+                    }
+                    catch (Exception ex) {
+                        string inner = ex.InnerException.Message;
+                        Console.WriteLine("Test run error: {0}", inner);
+                    }
+                }
+
+                return 1;
+            });
+            app.Execute(args);
+        }
+    }
+}
---
+++ cs/hrk/10-days-stats/day1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Int32;
+
+namespace hrk {
+    public class day1 {
+        static int[] ReadAllInts(string s, int? max = null) => Array.ConvertAll(s.Split(' ', max ?? Int32.MaxValue, StringSplitOptions.RemoveEmptyEntries), Parse);
+        public static void MainTest(string[] args) {
+            int n = Int32.Parse(Console.ReadLine());
+            int[] arr = ReadAllInts(Console.ReadLine(), n);
+            int sum = 0;
+            int model = 0;
+            int prev = -1;
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length; i++) {
+                sum += arr[i];
+                if (prev == arr[i] && model < prev) {
+                    model = prev;
+                }
+                else prev = arr[i];
+            }
+            float mean = ((float)sum) / n;
+            float median = n % 2 == 0 ? (arr[n / 2 - 1] + arr[n / 2]) / (float)2 : arr[n / 2];
+            if (model == 0 && n > 0) model = arr[0];
+
+            Console.WriteLine($"{mean:F1}");
+            Console.WriteLine($"{median:F1}");
+            Console.WriteLine($"{model}");
+            Console.ReadKey();
+        }
+    }
+}
+
+
